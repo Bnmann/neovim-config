7742b3fbfa41c2da8f717a907036a381ecd06338
Bartlomiej Neumann <7427410+Bnmann@users.noreply.github.com>
2023-01-29 17:36:05 +0100

    LSP Update

--- lua/vnconfig/lsp.lua
+++ lua/vnconfig/lsp.lua
@@ -1,74 +1,58 @@
-local function on_attach_override(client, bufnr)
-	require("lsp_signature").on_attach(client, bufnr)
-	-- vim.lsp.handler
+local lsp = require("lsp-zero")
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+	"pylsp",
+	"pyright",
+	"clangd",
+})
+
+lsp.configure('clangd', require("clangd_extensions").prepare())
+
+lsp.setup()
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	update_in_insert = false,
+	underline = true,
+	severity_sort = true,
+	float = true,
+})
+
+local sagacmd = function(cmd)
+    vim.cmd('Lspsaga ' .. cmd)
 end
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-	properties = {
-		"documentation",
-		"detail",
-		"additionalTextEdits",
-	},
-}
+function LspActions()
+    sagacmd('code_action')
+end
 
-require("clangd_extensions").setup({
-	server = {
-		on_attach = on_attach_override,
-		root_dir = function()
-			return vim.loop.cwd()
-		end,
-		capabilities = capabilities,
-	},
-})
+function LspRename()
+    sagacmd('rename')
+end
 
-require("lspconfig").pylsp.setup({})
-require("lspconfig").pyright.setup({})
-require("lspconfig").gopls.setup({})
-require("lspconfig").jdtls.setup({
-})
+function LspFinder()
+    sagacmd('lsp_finder')
+end
 
-local cmp = require("cmp")
-cmp.setup({
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "vsnip" },
-		{ name = "path" },
-		{ name = "calc" },
-		{ name = "buffer" },
-	}),
-})
+local nnoremap = function(seq, callback)
+    vim.keymap.set('n', seq, callback, { silent = true })
+end
 
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline("/", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = "buffer" },
-	},
-})
 
-require("lspsaga").init_lsp_saga()
+require("lspsaga").setup({})
 
 vim.api.nvim_create_user_command("LspActions", function()
-	require("lspsaga.codeaction").code_action()
+    LspActions()
 end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("LspRename", function()
-	require("lspsaga.rename").rename()
+    LspRename()
+end, { nargs = 0 })
+
+vim.api.nvim_create_user_command('LspFinder', function()
+    LspFinder()
 end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("LspFormat", function()
@@ -76,3 +60,7 @@ vim.api.nvim_create_user_command("LspFormat", function()
 		vim.lsp.buf.formatting_sync(nil, 1000)
 	end
 end, { nargs = 0 })
+
+nnoremap('<C-.>', LspActions)
+nnoremap('<C-,>', LspFinder)
+nnoremap('<A-o>', function() vim.cmd('ClangdSwitchSourceHeader') end)
--- lua/vnconfig/plugins.lua
+++ lua/vnconfig/plugins.lua
@@ -19,25 +19,37 @@ require("packer").startup(function(use)
 	use("nvim-lua/plenary.nvim")
 	use("nvim-lua/popup.nvim")
 
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-calc")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-path")
-
-	use("L3MON4D3/LuaSnip")
-	use("saadparwaiz1/cmp_luasnip")
-
 	use("marko-cerovac/material.nvim")
 	use("mhartington/oceanic-next")
 
 	use({
-		"neovim/nvim-lspconfig",
+		"VonHeikemen/lsp-zero.nvim",
 		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-calc" },
+			{ "hrsh7th/cmp-cmdline" },
+			{ "hrsh7th/cmp-path" },
+
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			-- Snippet Collection (Optional)
+			{ "rafamadriz/friendly-snippets" },
+
 			{ "p00f/clangd_extensions.nvim" },
-			{ "tami5/lspsaga.nvim" },
-			{ "ray-x/lsp_signature.nvim" },
+			{ "glepnir/lspsaga.nvim" },
 		},
 	})
 
