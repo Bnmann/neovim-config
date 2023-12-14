94ba87ed0ec2c98712a1836c00c53956dbb43f64
Bartlomiej Neumann <7427410+Bnmann@users.noreply.github.com>
2023-12-14 14:18:14 +0100

    Remove SHIFT+arrow bindings

--- lua/vnconfig/init.lua
+++ lua/vnconfig/init.lua
@@ -7,3 +7,17 @@ require("vnconfig.lsp")
 require("vnconfig.tree")
 require("vnconfig.colorscheme")
 require("vnconfig.terminal")
+
+
+vim.api.nvim_set_keymap('i', '<S-Up>', '', {})
+vim.api.nvim_set_keymap('i', '<S-Down>', '', {})
+
+vim.api.nvim_set_keymap('n', '<S-Up>', '', {})
+vim.api.nvim_set_keymap('n', '<S-Down>', '', {})
+
+vim.api.nvim_set_keymap('v', '<S-Up>', '', {})
+vim.api.nvim_set_keymap('v', '<S-Down>', '', {})
+
+vim.api.nvim_set_keymap('i', '<S-CR>', '', {})
+vim.api.nvim_set_keymap('n', '<S-CR>', '', {})
+vim.api.nvim_set_keymap('v', '<S-CR>', '', {})
